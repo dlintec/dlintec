521f205bef869024ba0b0429bc7ea5252040efd0
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2017-08-28 12:03:26 -0500

    mobile config

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,7 @@
 // inside mobile-config.js
 App.info({
   name: 'dlintec',
-  description: 'An Android app built with Meteor',
+  description: 'Multiplatform App',
   version: '0.0.1',
   buildNumber: '101' // has to be a number
 });

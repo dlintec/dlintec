66481d0b93024c347f0745f926ed3ce0a5f552dc
tadeogutierrez <tadeogutierrezsanchez@gmail.com>
2017-09-08 17:41:09 -0500

    title

--- client/template_logic/flow_layout.js
+++ client/template_logic/flow_layout.js
@@ -6,6 +6,6 @@ import React from 'react';
 Template.flow_layout.onCreated(function() {
   this.autorun(() => {
     FlowRouter.watchPathChange();
-    document.title = orion.dictionary.get('site.title', '...');
+    document.title = orion.dictionary.get('site.title', 'dlintec app');
   });
 });
